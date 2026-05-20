cbca0b75ccfeb8f8725694b6f230490cfc7218c6
ChillDanger <danielalswanger@gmail.com>
2026-05-20 17:42:42 -0400

    tab

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
+<h1 className="text-4xl font-bold text-aquamarine-800 dark:text-gray-200">
         Daniel Alswanger
       </h1>
     </div>
   );
 }
+
